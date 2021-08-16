f17e4823b26453403fc2b0bb749aa2f8942d0664
AmerAbuZaiton123 <amerabuzaiton@outlook.com>
2021-08-17 02:41:23 +0300

    dont marge this branch

--- app.js
+++ app.js
@@ -3,31 +3,30 @@ let formEle = document.getElementById('Form');
 let mainEle = document.getElementById('main');
 let fieldset1 = document.getElementById('fieldset');
 
-
-function Registration(f_Name, l_Name, fa_Name, ma_Name, National, Num_National, Phone, Email, Address, Age, Gender, Healthy) {
+function Registration(f_Name, l_Name, fa_Name, ma_Name, National, Num_National, Phone,Email,Address,   Age, Gender, healthy) {
   this.f_Name = f_Name
   this.l_Name = l_Name
   this.fa_Name = fa_Name
   this.ma_Name = ma_Name
-  this.Nationa = National
-  this.Num_Nationa = Num_National
-  this.Phone = Phone
+  this.National = National
+  this.Num_National = Num_National
+  this.Phone = Phone 
   this.Email = Email
   this.Address = Address
   this.Age = Age
-  this.Gender = Gender
-  this.Healthy = Healthy
-
+  this.Gender=Gender
+  this.healthy=healthy
   Registration.obj.push(this)
 }
 
-
-
 Registration.obj = []
+getItem();
+
 
 main.addEventListener("submit", FormMethod)
 function FormMethod(e) {
   e.preventDefault();
+  
 
   let value1 = e.target.f_Name.value;
   let f_Name = validateFormName(value1)
@@ -40,36 +39,46 @@ function FormMethod(e) {
 
   let value4 = e.target.ma_Name.value;
   let ma_Name = validateFormName(value4)
-
+ 
   let value5 = e.target.National.value;
   let National = validateFormName(value5)
-
-  let Address = e.target.Address.value;
-  let Email = e.target.Email.value;
-
+  
+ 
+ 
+  let check2 = document.getElementById('Num_National').value;
+  let id1 = 'Num_National';
+  let Num_National = validateFormNumber(check2, id1);
   let check1 = document.getElementById('Phone').value
   let id = 'Phone'
   let Phone = validateFormNumber(check1, id);
+  
+  let Email =  e.target.Email.value;
 
-  let check2 = document.getElementById('National_Num').value;
-  let id1 = 'National_Num';
-  let National_Num = validateFormNumber(check2, id1);
+  let Address = e.target.Address.value;
 
-  let Age = e.target.Age.value;
+ let Age = e.target.Age.value;
+
   var select = document.getElementById('Gender');
   var Gender = select.options[select.selectedIndex].value;
+
   let healthy = e.target.healthy.checked;
- 
+
+  
+
+
+  
   //new Object
-  let newRegistration = new Registration(f_Name, l_Name, fa_Name, ma_Name, Address, Email, Phone, National, National_Num, Age, Gender, healthy);
+  let newRegistration = new Registration(f_Name, l_Name, fa_Name, ma_Name, National, Num_National,Phone, Email,Address,   Age, Gender, healthy);
   //if i need LocalStorge this is code convert ob to string and but to local Storge and when i need use must convert to object agein
-  localStorage.data = JSON.stringify(Registration.obj);
+  let savedData = JSON.stringify(Registration.obj)
+  //  localStorage.data = JSON.stringify(Registration.obj);
+  localStorage.setItem('Regist', savedData)
  
   DeleteForm();
   Rendertbl();
   RenderTable2();
-}
-
+  rendringlist()
+ }
 //  function DeleteForm()
 function DeleteForm() {
   if (counter = 2) {
@@ -80,15 +89,13 @@ function DeleteForm() {
     // }
   }
 }
-
-
 //Rendertbl
 let divtbl = document.getElementById('tbl')
 function Rendertbl() {
-  let data = JSON.parse(localStorage.data);
-
+  // for(let i=0 ;i<Registration.obj.length;i++)
+  // 
   let tr1 = document.createElement('tr');
-  tbl.appendChild(tr1)
+  divtbl.appendChild(tr1)
 
   let th1 = document.createElement('th');
   th1.textContent = 'Student ';
@@ -107,18 +114,20 @@ function Rendertbl() {
 
 
   let tr2 = document.createElement('tr');
-  tbl.appendChild(tr2)
+  divtbl.appendChild(tr2)
   let td1 = document.createElement('td');
   td1.textContent = 'Student Name';
   tr2.appendChild(td1)
   let td2 = document.createElement('td');
-  td2.textContent = Registration.obj[0].f_Name + "" + Registration.obj[0].l_Name
+  td2.textContent = Registration.obj[0].f_Name + "  " + Registration.obj[0].l_Name
   tr2.appendChild(td2)
   let td3 = document.createElement('td');
+  //
   td3.textContent = ' Address  ';
   tr2.appendChild(td3)
   let td4 = document.createElement('td');
   td4.textContent = Registration.obj[0].Address;
+  
   tr2.appendChild(td4)
 
 
@@ -129,10 +138,10 @@ function Rendertbl() {
   td31.textContent = 'Parent Name ';
   tr3.appendChild(td31)
   let td32 = document.createElement('td');
-  td32.textContent = Registration.obj[0].fa_Name + "" + Registration.obj[0].ma_Name
+  td32.textContent = Registration.obj[0].fa_Name + "\t" + Registration.obj[0].ma_Name
   tr3.appendChild(td32)
   let td33 = document.createElement('td');
-  td33.textContent = ' Email';
+  td33.textContent = 'Email';
   tr3.appendChild(td33)
   let td34 = document.createElement('td');
   td34.textContent = Registration.obj[0].Email;
@@ -145,7 +154,7 @@ function Rendertbl() {
   td41.textContent = ' National Number';
   tr4.appendChild(td41)
   let td42 = document.createElement('td');
-  td42.textContent = Registration.obj[0].Num_Nationa;
+  td42.textContent = Registration.obj[0].Num_National;
   tr4.appendChild(td42)
   let td43 = document.createElement('td');
   td43.textContent = ' Phone';
@@ -161,13 +170,13 @@ function Rendertbl() {
   td51.textContent = ' National';
   tr5.appendChild(td51)
   let td52 = document.createElement('td');
-  td52.textContent = Registration.obj[0].Nationa;
+  td52.textContent = Registration.obj[0].National;
   tr5.appendChild(td52)
   let td53 = document.createElement('td');
   td53.textContent = 'Number of Student';
   tr5.appendChild(td53)
   let td54 = document.createElement('td');
-  td54.textContent = Registration.obj.length ;
+  td54.textContent = Registration.obj.length;
   tr5.appendChild(td54)
 
   let tr6 = document.createElement('tr');
@@ -179,49 +188,61 @@ function Rendertbl() {
   td62.textContent = '300 JD';
   tr6.appendChild(td62)
   let td63 = document.createElement('td');
-  td63.textContent = 'Student Number';
+  td63.textContent = 'Student id';
   tr6.appendChild(td63)
   let td64 = document.createElement('td');
-  td64.textContent = data.length;
+  td64.textContent = randomNumber()
   tr6.appendChild(td64)
+  //}
 
 }
+let table2 = document.getElementById('tbl2')
+function RenderTable2() {
+  let tr1 = document.createElement('tr')
+  table2.appendChild(tr1)
 
-let table2=document.getElementById('tbl2')
-function RenderTable2()
-{
-  let tr1=document.createElement('tr')
-   table2.appendChild(tr1)
+  let th1 = document.createElement('th')
+  th1.textContent = 'Student Name'
+  tr1.appendChild(th1)
 
-   let th1=document.createElement('th')
-   th1.textContent='Student ID'
-   tr1.appendChild(th1)
+  //  let th3=document.createElement('th')
+  //  th3.textContent='Student Of Number'
+  //  tr1.appendChild(th3)
+  for (let i = 0; i < Registration.obj.length; i++) {
+    let tr2 = document.createElement('tr')
+    table2.appendChild(tr2)
 
-   let th2=document.createElement('th')
-   th2.textContent='Student Name'
-   tr1.appendChild(th2)
+    let td2 = document.createElement('td');
+    td2.textContent = Registration.obj[i].f_Name + " \t" + Registration.obj[i].fa_Name + " \t " + Registration.obj[i].l_Name
+    tr2.appendChild(td2)
 
-   let th3=document.createElement('th')
-   th3.textContent='Student Of Number'
-   tr1.appendChild(th3)
+
+    // let td4 = document.createElement('td');
+    // td4.textContent = Registration.obj[i].length;
+    // tr2.appendChild(td4)
+
+
 
   }
-
+}
+function randomNumber() {
+  return (Math.floor(Math.random() * (9999999999 - 1111111111 + 1) + 1111111111));
+}
 // std class
- let counter = 0;
+let counter = 0;
 // let stdCounter = 0;
-// function counter(stdCounter)
-// if (stdCounter < 30) {
-//   return "Class A"+""+stdCounter;
+// function counter(x)
+// if (x < 30) {
+//   return "Class A"+""+x;
 // }
-// else if (stdCounter < 60) {
-//   return "Class B"+""+stdCounter
+// else if (x < 60) {
+//   return "Class B"+""+x
 // }
-// else if (stdCounter < 90) {
-//   return "Class B"+""+stdCounter
+// else if (x < 90) {
+//   return "Class B"+""+x
 // }
-// else if (stdCounter < 120) {
-//   return "Class B"+""+stdCounter
+// else if (x < 120) {
+//   return "Class B"+""+x
 // }
 
 
@@ -254,4 +275,63 @@ function validateFormNumber(testNumber, id) {
 
   }
 
+
+}
+function getItem() {
+  let item = localStorage.getItem('Regist');
+  let displayItem = JSON.parse(item)
+  if (displayItem && displayItem.length) {
+    Registration.obj = displayItem
+    return;
+  }
+
+}
+let divlist=document.getElementById('list')
+
+function rendringlist()
+{  
+  let h1E=document.createElement('h1');
+  
+   divlist.appendChild(h1E);
+
+   let olE1=document.createElement('ol');
+    divlist.appendChild(olE1);
+
+    let liE1=document.createElement('li');
+     liE1.textContent ='Birth date certificate'
+     olE1.appendChild(liE1);
+ 
+     let liE2=document.createElement('li');
+     liE2.textContent ='Two Personal Photo'
+     olE1.appendChild(liE2);
+ 
+     let liE3=document.createElement('li');
+     liE3.textContent ='Vaccine certificates'
+     olE1.appendChild(liE3);
+     let liE4=document.createElement('li');
+     liE4.textContent ='parents personal identification'
+     olE1.appendChild(liE4);
+
+     let h2E=document.createElement('h1');
+     divlist.appendChild(h2E);
+
+    let olE2=document.createElement('ol');
+    divlist.appendChild(olE2);
+
+     let liE11=document.createElement('li');
+     liE11.textContent ='Birth date certificate'
+     olE2.appendChild(liE11);
+ 
+     let liE12=document.createElement('li');
+     liE12.textContent ='Two Personal Photo'
+     olE2.appendChild(liE12);
+ 
+     let liE13=document.createElement('li');
+     liE13.textContent ='Vaccine certificates'
+     olE2.appendChild(liE13);
+     
+     let liE14=document.createElement('li');
+     liE14.textContent ='parents personal identification'
+     olE2.appendChild(liE14);
+ 
 }
